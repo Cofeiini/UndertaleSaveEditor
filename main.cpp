42b2558eb1b27b0553b28ce80b2299171ce3db37
Cofeiini <Cofeiini@gmail.com>
2015-12-17 22:27:29 +0200

    Graduated to version 0.1. Yay.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	QApplication mApp(argc, argv);
 	mApp.setOrganizationName("Cofeiini");
 	mApp.setApplicationName("Undertale Save Editor");
-	mApp.setApplicationVersion("0.0");
+	mApp.setApplicationVersion("0.1");
 	mApp.setWheelScrollLines(1);
 	MainWindow mWindow;
 	mWindow.show();
